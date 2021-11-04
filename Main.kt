7da7cffd74dd84c945b9b39324c359372f21ec0d
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-11-04 08:05:39 -0300

    Atividade 03/11

--- Main.kt
+++ Main.kt
@@ -1,29 +1,7 @@
-fun main(args: Array<String>) {
+{
+    val primeiroNome = ""
+    var ultimoNome = ""
+    var ra = 0
+    var nota = 0
+    var ano = 0
 
-    adicao(1, 2)
-    subtracao(3,4)
-    multiplicacao(5,6)
-    divisao(7,8)
-    potencia
-}
-
-   fun adicao( num1: int, num2: int){
-       var result = num1 + num2
-       print("o resultado da adição é: $result ")
-   }
-    fun subtracao ( num1: int, num2: int){
-        var result =num1 - num2
-        print("o resultado da subtração é: $result")
-    }
-    fun multiplicacao (num1: int, num2: int) {
-        var result =num1 * num2
-        println("o resultado da multiplicação é:$result")
-        }
-    fun divisao(num1: double, num2: double){
-        var result = num1 / num 2
-        println("o resultado da divisão é: $result")
-    }
-    fun potencia(num1: double, num2: double){
-        var result = num1 / num2
-        println("o resultado da potencia é: $result")
-    }
---
+++ curso.kt
@@ -0,0 +1,15 @@
+class curso {
+    var nomeCurso = ""
+    var nomeProfessor = ""
+    var ano = 0
+    var lista = mutableListOf("cassiano")
+
+}
+    fun inscrever(nome: String) {
+        println("digite seu nome:")
+    var nome = readLine()!!
+    var lista.add(nome).tostring()
+        {
+
+        }
+    }
